5172417f25b7a6182ebce4b7792c8d4c55b4fd58
OscarMiraG <121115694+OscarMiraG@users.noreply.github.com>
2022-12-27 10:07:08 -0600

    Lo de la replica como en las batallas de gallos

--- Proyecto_Final.R
+++ Proyecto_Final.R
@@ -5,6 +5,9 @@ Datosos <- read.csv("Datos/Datos_Proyecto.csv")
 Datosos 
 
 ELISA <- function(x){
+  numero <- readline(prompt = "Cuántas replicas se realizaron?: ")
+  as.numeric(numero)
+  if (numero == 3){
   library (ggplot2)
   library (Rmisc)
   Promedios <- matrix (nrow = 12, ncol = 2)
@@ -113,10 +116,139 @@ ELISA <- function(x){
     geom_point(position=pd)
   
   print (Grafica_Perrona)
+  }
+}else if (numero == 2){
+  library (ggplot2)
+  library (Rmisc)
+  Promedios <- matrix (nrow = 12, ncol = 2)
+  colnames(Promedios) <- c ("Preinmunizado","Postinmunzado")
+  row.names(Promedios) <- c ("1:500", "1:100", "1:2000", "1:4000", "1:8000", 
+                             "1;16000", "1:32000", "1:64000", "1:128000", 
+                             "1:256000", "1:512000", "1:1024000")
+  
+  
+  Promedios [1,1] <- (x[1,1] + x [2,1])/2
+  Promedios [2,1] <- (x[1,2] + x [2,2])/2
+  Promedios [3,1] <- (x[1,3] + x [3,3])/2
+  Promedios [4,1] <- (x[1,4] + x [2,4])/2
+  Promedios [5,1] <- (x[1,5] + x [2,5])/2
+  Promedios [6,1] <- (x[1,6] + x [2,6])/2
+  Promedios [7,1] <- (x[1,7] + x [2,7])/2
+  Promedios [8,1] <- (x[1,8] + x [2,8])/2
+  Promedios [9,1] <- (x[1,9] + x [2,9])/2
+  Promedios [10,1] <- (x[1,10] + x [2,10])/2
+  Promedios [11,1] <- (x[1,11] + x [2,11])/2
+  Promedios [12,1] <- (x[1,11] + x [2,11])/2
+  
+  Promedios [1,2] <- (x[3,1] + x [4,1])/2
+  Promedios [2,2] <- (x[3,2] + x [4,2])/2
+  Promedios [3,2] <- (x[3,3] + x [4,3])/2
+  Promedios [4,2] <- (x[3,4] + x [4,4])/2
+  Promedios [5,2] <- (x[3,5] + x [4,5])/2
+  Promedios [6,2] <- (x[3,6] + x [4,6])/2
+  Promedios [7,2] <- (x[3,7] + x [4,7])/2
+  Promedios [8,2] <- (x[3,8] + x [4,8])/2
+  Promedios [9,2] <- (x[3,9] + x [4,9])/2
+  Promedios [10,2] <- (x[3,10] + x [4,10])/2
+  Promedios [11,2] <- (x[3,11] + x [4,11])/2
+  Promedios [12,2] <- (x[3,12] + x [4,12])/2
+  
+  Control_Positivo <- (x[5,1] + x [5,2] + x [6,1] + x [6,2])/4
+  Control_Negativo <- (x[5,3] + x [5,4] + x [6,3] + x [6,4])/4
+  Conjugado <- (x[5,5] + x [5,6] + x [6,5] + x [6,6])/4
+  
+  print (Promedios)
+  print (Control_Negativo)
+  print (Control_Positivo)
+  print (Conjugado)
+  
+  Promedios2 <- matrix (nrow = 84, ncol = 3)
+  colnames(Promedios2) <- c ("Absorbancia", "Muestra", "Dilucion")
+  
+  Promedios2 <- data.frame(Promedios2)
+  
+  Promedios2 [1:24,1] <- c (x [3,1], x [4,1], x [3,2], x [4,2], x[3,3], x [4,3],
+                            x [3,4], x [4,4], x [3,5], x [4,5], x [3,6], 
+                            x [4,6], x [3,7], x [4,7], x [3,8], x [4,8],
+                            x [3,9], x [4,9], x [3,10], x [4,10], x [3,11], x [4,11], 
+                            x [3,12], x [4,12])
+  
+  Promedios2 [25:72,1] <- c (x[7,3], x [7,4], x [8,3], x [8,4])
+  
+  
+  
+  Promedios2 [1:72,3] <- c ("A", "A", 
+                            "B", "B",
+                            "C", "C",
+                            "D", "D", 
+                            "E", "E", 
+                            "F", "F", 
+                            "G", "G",
+                            "H", "H",
+                            "I",  "I", 
+                            "J", "J",
+                            "K", "K", 
+                            "L", "L", 
+                            "A", "A", "A", "A", 
+                            "B", "B", "B", "B",
+                            "C", "C", "C", "C",
+                            "D", "D", "D", "D", 
+                            "E", "E", "E", "E", 
+                            "F", "F", "F", "F", 
+                            "G", "G", "G", "G",
+                            "H", "H", "H", "H",
+                            "I",  "I",  "I", "I", 
+                            "J", "J", "J", "J",
+                            "K", "K", "K", "K", 
+                            "L", "L", "L", "L")
+  
+  for (i in 1:24){
+    Frase <- "PostInmunizado"
+    Promedios2 [1:36,2] <- Frase
+    
+  }
+  
+  for (i in 1:47){
+    Frase2 <- "ControlNegativo"
+    Promedios2 [25:72,2] <- Frase2
+  }
+  
+  print (Promedios2)
+  
+  Resumen <- summarySE (Promedios2, measurevar = "Absorbancia", groupvars = c ("Dilucion", "Muestra"))
+  
+  print (Resumen)
+  
+  pd <- position_dodge(0.1)
+  Grafica_Perrona <- ggplot(data= Resumen, aes(x = Dilucion, y = Absorbancia, group = Muestra, colour=Muestra)) +
+    geom_errorbar(aes(ymin = Absorbancia - se, ymax = Absorbancia + se), width=.1, position=pd) +
+    geom_line(position=pd) +
+    geom_point(position=pd)
+  
+  print (Grafica_Perrona)
   
 }
 
+
+
+
+
 ELISA(Datosos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     attach(Datosos)
     
